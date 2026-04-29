d428bde6883fa07a1d41c6fb4a20fde5d2bdd1a0
adammKass <adam.kascak@protonmail.com>
2026-04-29 02:30:58 +0200

    -astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://adammKass.github.io",
+  base: "/nierproject",
   vite: {
     plugins: [tailwindcss()],
   },
